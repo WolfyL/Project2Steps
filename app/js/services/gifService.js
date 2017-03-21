350fad239a0864336869f8bfa65850a0b52590c5
Wolfy <s.leheup@gmail.com>
2017-03-21 18:20:23 +0100

    search is ok (gotta fix img size)

--- app/js/services/gifService.js
+++ app/js/services/gifService.js
@@ -1,17 +1,26 @@
 angular.module('app')
-    .service('UserService', function($http) {
+    .service('GifService', function($http) {
         return {
             getAll: function() {
-                return $http.get('/users');
+                return $http.get('http://api.giphy.com/v1/gifs/search?q=sexy+girl&api_key=dc6zaTOxFJmzC');
+            },
+            getX: function(n) {
+                return $http.get('http://api.giphy.com/v1/gifs/search?q=sexy+girl&api_key=dc6zaTOxFJmzC&limit=' + n);
             },
             getOne: function(id) {
-                return $http.get('/users/' + id);
+                return $http.get('http://api.giphy.com/v1/gifs/search?q=sexy+girl&api_key=dc6zaTOxFJmzC' + id);
+            },
+            getSearch : function(search){
+                return $http.get('http://api.giphy.com/v1/gifs/search?q=' + search + '&api_key=dc6zaTOxFJmzC')
+            },
+            getLucky: function() {
+                return $http.get('http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC');
             },
             update: function(id, user) {
-                return $http.put('/users/' + id, user);
+                return $http.put('http://api.giphy.com/v1/gifs/search?q=sexy+girl&api_key=dc6zaTOxFJmzC' + id, user);
             },
             delete: function(id) {
-                return $http.delete('/users/' + id);
+                return $http.delete('http://api.giphy.com/v1/gifs/search?q=sexy+girl&api_key=dc6zaTOxFJmzC' + id);
             }
         };
     });
