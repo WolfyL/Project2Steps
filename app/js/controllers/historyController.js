b23692e144e914469e59a07a97fa1b8a596fabe0
jordan-couard <jordan.couard@gmail.com>
2017-03-28 10:57:55 +0200

    history

--- app/js/controllers/historyController.js
+++ app/js/controllers/historyController.js
@@ -1,5 +1,23 @@
 angular.module('app')
-    .controller('HistoryController', function($scope, GifService) {
+    .controller('HistoryController', function($scope, HistoryService) {
+        Directives.directive("passwordVerify", function() {
+            return {
+                require: "ngModel",
+                link: function(scope, element, attrs, ctrl) {
+                    ctrl.$parsers.unshift(function(viewValue) {
+                        var origin = scope.$eval(attrs["passwordVerify"]);
+                        if (origin !== viewValue) {
+                            ctrl.$setValidity("passwordVerify", false);
+                            return undefined;
+                        } else {
+                            ctrl.$setValidity("passwordVerify", true);
+                            return viewValue;
+                        }
+                    });
 
+                }
+            };
+            angular.module.directive("compareTo", compareTo);
+        });
 
     });
---
+++ app/js/services/historyService.js
@@ -0,0 +1,6 @@
+angular.module('app')
+    .service('HistoryService', function($http) {
+        return {
+
+        };
+    });
