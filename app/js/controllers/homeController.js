13180d89e36c53ca1b8ecb0e35a4156f1cbac0c7
Wolfy <s.leheup@gmail.com>
2017-03-29 19:55:06 +0200

    Modal finished at the right place

--- app/js/controllers/homeController.js
+++ app/js/controllers/homeController.js
@@ -1,7 +1,3 @@
-angular.module("app")
-.controller('MyCtrl', ['$scope', function($scope) {
-        $scope.modalShown = false;
-        $scope.toggleModal = function() {
-            $scope.modalShown = !$scope.modalShown;
-        };
-    }]);
+// angular.module("app")
+// .controller('MyCtrl', ['$scope', function($scope) {
+//     }]);
--- app/js/controllers/mainController.js
+++ app/js/controllers/mainController.js
@@ -1,9 +1,29 @@
 angular.module('app')
 
     .controller('MainController', function($scope, GifService, VoteService, $location) {
+
         var n = 0;
         $scope.getSearch = [];
         $scope.lucky = [];
+
+        $scope.modalShown = false;
+        $scope.toggleModal = function() {
+            $scope.modalShown = !$scope.modalShown;
+        };
+
+        $scope.loupe = function () {
+          $scope.modalShown = !$scope.modalShown;
+        };
+
+        $scope.search = {saisie:""};
+        $scope.launchSearch = function(){
+          var saisie = $scope.search.saisie;
+          console.log('saisie', saisie);
+          if (saisie !== undefined) {
+              console.log(saisie);
+              $location.path('/search/' + saisie);
+          }
+        };
 
         function randomGif() {
             GifService.getLucky().then(function(res) {
@@ -24,14 +44,6 @@ angular.module('app')
             });
         };
 
-
-        $scope.loupe = function PromptMessage() {
-            var saisie = prompt("Saisissez votre texte :", "");
-            if (saisie !== null) {
-                console.log(saisie);
-                $location.path('/search/' + saisie);
-            }
-        };
 
 
         $scope.copy = function() {
@@ -62,7 +74,7 @@ angular.module('app')
 
 
         $scope.goSearch = function() {
-            search = $scope.search;
+            search = $scope.search.saisie;
             console.log(search);
             GifService.getSearch(search).then(function(res) {
                 var i = Math.floor(Math.random(0, 101) * 100);
