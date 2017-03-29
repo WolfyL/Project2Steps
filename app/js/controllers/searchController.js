fb75db0ef03b77f697a4841aa3286504246d3dc9
Wolfy <s.leheup@gmail.com>
2017-03-29 21:23:30 +0200

    MEP template search, adjust btn fct, keep the theme. All good

--- app/js/controllers/searchController.js
+++ app/js/controllers/searchController.js
@@ -1,9 +1,79 @@
 angular.module('app')
-    .controller('SearchController', function($scope, GifService, $stateParams) {
+    .controller('SearchController', function($scope, GifService, $stateParams, VoteService, $location) {
         console.log($stateParams.query);
         GifService.getSearch($stateParams.query).then(function(res) {
             var i = Math.floor(Math.random(0, res.data.data.length) * 100);
             $scope.getSearch = res.data.data[i];
             console.log($scope.getSearch);
         });
+
+        var n = 0;
+        $scope.getSearch = [];
+        $scope.lucky = [];
+
+        $scope.modalShown = false;
+        $scope.toggleModal = function() {
+            $scope.modalShown = !$scope.modalShown;
+        };
+
+        $scope.loupe = function () {
+          $scope.modalShown = !$scope.modalShown;
+        };
+
+        $scope.search = {saisie:""};
+        $scope.launchSearch = function(){
+          var saisie = $scope.search.saisie;
+          console.log('saisie', saisie);
+          if (saisie !== undefined) {
+              console.log(saisie);
+              $location.path('/search/' + saisie);
+          }
+        };
+
+        $scope.copy = function() {
+            var toCopy = document.getElementById('to-copy'),
+                btnCopy = document.getElementById('copy');
+
+            toCopy.select();
+            document.execCommand('copy');
+            return false;
+        };
+
+        function themeGif() {
+          GifService.getSearch($stateParams.query).then(function(res) {
+              var i = Math.floor(Math.random(0, res.data.data.length) * 100);
+              $scope.getSearch = res.data.data[i];
+              console.log($scope.getSearch);
+            });
+        }
+
+
+        $scope.addDislike = function() {
+            VoteService.updateDislike($scope.gifId, +1).then(function(res) {
+            });
+
+            themeGif();
+        };
+
+        $scope.addLike = function() {
+            VoteService.updateLike($scope.gifId, +1).then(function(res) {
+            });
+
+            themeGif();
+        };
+        // themeGif();
+
+
+
+        $scope.goSearch = function() {
+            search = $scope.search.saisie;
+            console.log(search);
+            GifService.getSearch(search).then(function(res) {
+                var i = Math.floor(Math.random(0, 101) * 100);
+                console.log(i);
+                $scope.getSearch = res.data.data[i];
+                console.log($scope.getSearch);
+            });
+        };
+
     });
